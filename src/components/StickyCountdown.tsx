92ebfba109bb0079a22424eae147eadc47c9a247
Vitor Oliveira <vbrazo@gmail.com>
2025-08-05 18:13:21 -0700

    change title

--- src/components/StickyCountdown.tsx
+++ src/components/StickyCountdown.tsx
@@ -20,7 +20,7 @@ const StickyCountdown = () => {
         <div className="flex items-center justify-center gap-1.5 py-2.5 md:py-1.5">
           <div className="flex flex-row emoji">
             <img src="https://cdn.croct.io/assets/icon/emoji/rocket.png" alt="Rocket emoji" className="root-18nkjmo w-4 h-4" />
-            &nbsp; I've just launched an AI Interview Coach!
+            &nbsp; I've just launched an AI Career Agent and Interview Coach!
           </div>
           <div className="flex items-center gap-1.5">
             <span className="flex items-center bg-transparent rounded-full border border-[#cbcdfe] hover:bg-[#cbcdfe] hover:text-[#4f46e5] h-6 px-3 text-[#f7f7ff] text-xs font-medium transition-colors duration-100 ease-out">
