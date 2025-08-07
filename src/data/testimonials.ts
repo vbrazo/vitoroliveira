ff90569a7128ecf30c37b7fb246ad79e90cbb43f
vbrazo <vbrazo@gmail.com>
2025-08-06 18:27:58 -0700

    add testimonial images

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -5,65 +5,76 @@ export type TestimonialType = {
   imgSrc: string;
 };
 
+import YJ from "/testimonials/yj.jpg";
+import Garry from "/testimonials/garry.jpg";
+import Leandro from "/testimonials/leandro.jpeg";
+import Anderson from "/testimonials/anderson.jpg";
+import Jan from "/testimonials/jan.jpeg";
+import Jesse from "/testimonials/jesse.jpeg";
+import Leonardo from "/testimonials/leonardo.jpeg";
+import Wagner from "/testimonials/wagner.jpeg";
+import Alexandre from "/testimonials/alexandre.jpg";
+import Victoria from "/testimonials/victoria.jpeg";
+
 export const TESTIMONIAL_DATA: TestimonialType[] = [
   {
     tempId: 0,
     testimonial: "Vitor demonstrated exceptional leadership at Paywith, building and managing high-performing teams of 20+ developers. His technical expertise in infrastructure design and PCI compliance, combined with his passion for mentorship and team development, was instrumental in Paywith's growth. His balanced approach to work and life set a positive example for the entire organization.",
     by: "Yee Jee Tso, CTO @ Paywith",
-    imgSrc: "https://m.media-amazon.com/images/M/MV5BODM3MGEyYmItNDk0MS00OTVjLTlkNDQtMjRkMzM1YmQ4NTE4XkEyXkFqcGc@._V1_.jpg",
+    imgSrc: YJ,
   },
   {
     tempId: 1,
     testimonial: "Vitor was instrumental in our early-stage growth, he mentored Plataforma Impact students and played a key role in helping us define sales and business strategies. His strategic insights and mentorship were invaluable in shaping our company's foundation.",
     by: "Garry Carrier, CEO @ Plataforma Impact",
-    imgSrc: "https://findinghumansintech.com/wp-content/uploads/2023/01/1656878268319.jpg",
+    imgSrc: Garry,
   },
   {
     tempId: 2,
     testimonial: "As Vice President of Engineering at Paywith, Vitor's leadership transformed our team. His genuine passion for mentorship and team development created a culture of continuous learning and high performance. His ability to celebrate team victories and drive professional growth resulted in consistently outstanding results.",
     by: "Leandro França, Co-Founder @ Krafters",
-    imgSrc: "https://media.licdn.com/dms/image/v2/C4D03AQEHTCBOFl_bHg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1639413908539?e=1757548800&v=beta&t=mFVcQbWo2nOPBnD55wSysisSza7BiI6rRaJitLPdZ74",
+    imgSrc: Leandro,
   },
   {
     tempId: 3,
     testimonial: "I met Vitor through open-source collaboration, where he generously shared his leadership expertise. His guidance on team organization and personal development was invaluable. He even invited me to join one of his leadership programs, creating opportunities for growth and networking. Vitor remains a trusted mentor I can always count on.",
     by: "Anderson Konzen, Staff Engineer @ Arvore de Livros",
-    imgSrc: "https://www.elixirconf.eu/assets/images/Anderson_Konzen.jpg",
+    imgSrc: Anderson,
   },
   {
     tempId: 4,
     testimonial: "I highly recommend Vitor as a coach for CTOs and startup founders. His guidance significantly improved my leadership and communication skills, while facilitating valuable networking opportunities.",
     by: "Jan Vicent Hoffbauer, CTO @ Fluento.ai",
-    imgSrc: "https://media.licdn.com/dms/image/v2/D4E03AQFajF7Js5FqXg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1694233415827?e=1757548800&v=beta&t=YO4byNyeNCn15fHmwOfbM_mRvfC_XlenmojdOK64k28",
+    imgSrc: Jan,
   },
   {
     tempId: 5,
     testimonial: "Vitor is one of the most adaptable and hardworking professionals I've met. He excels at wearing multiple hats, approaches challenges with a positive attitude, and is always eager to learn. His collaborative spirit and supportive nature make him an exceptional coworker.",
     by: "Jesse Gonzalez, Senior Account Executive @ Gun.io",
-    imgSrc: "https://media.licdn.com/dms/image/v2/D5603AQHH_uAEDYcu8w/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1725788671058?e=1757548800&v=beta&t=m_J5rBKT9OUwaHGspl1vt6ECrpckBYsw2Y9Qk0zzb8c",
+    imgSrc: Jesse,
   },
   {
     tempId: 6,
     testimonial: "One of the best people that I've worked with. Excellent professional with high resilience! I've learnt a lot about Ruby on Rails and architecture design.",
     by: "Leonardo Ribeiro, CEO @ Verstand",
-    imgSrc: "https://avatars.githubusercontent.com/u/7218334?v=4",
+    imgSrc: Leonardo,
   },
   {
     tempId: 7,
     testimonial: "Vitor is an exceptional developer and talented entrepreneur. His technical skills and mindset drive solutions to their best. I've seen him build robust, well-tested Ruby on Rails code in record time. He's a top-notch engineer and businessperson who would be an outstanding addition to any team.",
     by: "Wagner Santos, Principal Engineer @ Moteefe",
-    imgSrc: "https://media.licdn.com/dms/image/v2/C4E03AQHEzbPspWfhNA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1517430622802?e=2147483647&v=beta&t=jEq-DRnNRuUxCu3DeHGZfgwmNb0OIZzBnl1CaJp9Kmc",
+    imgSrc: Wagner,
   },
   {
     tempId: 8,
     testimonial: "Vitor combines technical excellence with founder mindset. His ability to balance technical debt with business growth, make data-driven decisions, and build scalable systems makes him an invaluable partner for any founder looking to build a sustainable tech business.",
     by: "Alexandre Cabral, CTO @ Banco BBM",
-    imgSrc: "https://d2gjqh9j26unp0.cloudfront.net/profilepic/e25048886a630eec72056344533ea944",
+    imgSrc: Alexandre,
   },
   {
     tempId: 9,
     testimonial: "Vitor is a thoughtful, strategic leader and creative problem solver. His ability to break down concepts and teach others, combined with his growth mindset, makes him an exceptional teammate and mentor.",
     by: "Victoria Stahr, Talent Growth @ Gun.io",
-    imgSrc: "https://media.licdn.com/dms/image/v2/C4E03AQEWIG5MF2q3rA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1629315203984?e=1757548800&v=beta&t=HHrFBfmHOhzhJNok4llx04Qgpmy3WJ53uPp0u-Ssdr4",
+    imgSrc: Victoria
   }
 ]; 
